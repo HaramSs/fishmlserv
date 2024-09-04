24456d53424c27ebdd461de4994f42193e4baf18
HaramSs <haram8274@gmail.com>
2024-09-04 11:35:12 +0900

    amend main.py

--- src/fishmlserv/main.py
+++ src/fishmlserv/main.py
@@ -5,6 +5,9 @@ import pickle
 
 app = FastAPI()
 
+model_path = get_model_path()
+with open(model_path, 'rb') as f:
+    fish_model = pickle.load(f)
 
 @app.get("/")
 def read_root():
@@ -59,10 +62,9 @@ def fish(length: float, weight: float):
         dict: 물고기 종류를 담은 딕셔너리
     """
     # 모델을 가져와보아요
-    model_path = get_model_path()
-    with open(model_path, 'rb') as f:
-        fish_model = pickle.load(f)
+
     fish_class = fish_model.predict([[length, weight]])
+
 #    fish_name = "몰라"
     if fish_class == 0:
         fish_name = "도미"
